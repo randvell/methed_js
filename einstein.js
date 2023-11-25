a805b896dfaddd8d3f480efa9dd9467cef8f2ab7
Nikita Zimin <n.zimin@aristos.pw>
2023-11-26 00:11:34 +0300

    Module 4 Lesson 2

---
+++ einstein.js
@@ -0,0 +1,4 @@
+const myWeight = 65;
+const speedLight = 3e8;
+
+console.log(`E = ${myWeight * speedLight ** 2}`)
---
+++ index.js
@@ -0,0 +1,27 @@
+// const quantity = 3;
+// const productName = 'Спортивные штаны Adidas';
+// const productCategory = 'Брюки';
+// const price = 1800;
+
+// console.log('Товар: ' + productName);
+// console.log('Общая стоимость: ' + price * quantity);
+
+const productName = requestValue('Наименование товара');
+const productCategory = requestValue('Категория товара')
+const quantity = requestValue('Кол-во товара', 'number');
+const price = requestValue('Цена за шт', 'number');
+
+function requestValue(valueName, type = 'string') {
+  const value = prompt(`${valueName}: `);
+  if (type === 'number' && !isNaN(value)) {
+    return value;
+  } else if (typeof(value) === type) {
+    return value;
+  }
+
+  alert('Введены некорректные данные!');
+  return requestValue(valueName, type);
+}
+
+console.log(`На складе ${quantity} единицы товара "${productName}" 
+на сумму ${price * quantity} деревянных`);
